fbc2d054312769c4fd6be67cebfee75ffbc3c900
Jungmingyu <vlxjalsrb@naver.com>
2022-07-12 23:15:09 +0900

    1day

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,7 +19,28 @@ public class MemberJpaRepository {
         return member;
     }
 
-    public Member find(Long id) {
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public Member find(Long id) {  //단건 조회
         return em.find(Member.class, id);
     }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);  //null일수도 있다 = ofNullable()
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    public List<Member> findAll() {  //전체 조회
+        //JPQL 사용
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
 }
