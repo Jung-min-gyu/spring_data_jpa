83a206d62be6e57bb30b0bde3c36b75d2f292df8
Jungmingyu <vlxjalsrb@naver.com>
2022-07-12 22:40:33 +0900

    Initial commit

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,47 @@
+package study.datajpa.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    protected Member() {
+//
+//    }
+//    jpa에서 엔티티는 기본 생성자가 하나 있어줘야 한다. 또 private으로 권한 설정을 하면 안된다. 최소 protected.
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
